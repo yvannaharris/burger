e23fe3de4dee18fc01e42129d41521810c764989
Yvanna Harris <yvannaharris@yvannas-air.attlocal.net>
2017-09-29 19:13:22 -0400

    Added main hadlebars file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO burgers (burger_name) VALUES ('Blue Cheese Bacon Burger');
 INSERT INTO burgers (burger_name) VALUES ('Mac & Cheese Burger');
-INSert INTO burgers (burger_name) VALUES ('Beakfast Burger');
+INSERT INTO burgers (burger_name) VALUES ('Beakfast Burger');
