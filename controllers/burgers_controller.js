0c68de14c3f93d00888f75371f753aa851e063de
Yvanna Harris <yvannaharris@yvannas-air.attlocal.net>
2017-09-29 20:11:17 -0400

    Updated controller file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,4 +3,40 @@ var burger = require("../models/burger.js");
 
 var router = express.Router();
 
+router.get("/", function(req, res) {
+	burger.all(function(data) {
+		var hbsObject = {
+			burgers: data
+		};
+		res.render("index", hbsObject);
+	});
+});
+
+router.post("/", function(req, res) {
+	burger.create([
+		"burger_name", "devoured"],
+		[req.body.burger_name, req.body.devoured],
+		function() {
+			res.redirect("/");
+		});
+});
+
+router.put(":id", function(req, res) {
+	var condition = "id = " + req.params.id;
+
+	burger.update({
+		devoured: req.body.devoured
+	}, condition, function() {
+		res.redirect("/";)
+	});
+});
+
+router.delete("/:id", function(req, res) {
+	var condition = "id = " + req.params.id;
+
+	burger.delete(condition, function() {
+		res.redirect("/");
+	});
+});
+
 module.exports = router;
